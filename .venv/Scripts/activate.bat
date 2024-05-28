710621bf755750d5ce44f95dad76a323d976c1b1
rostislav <bileckijrostislav46@gmail.com>
2024-05-28 23:49:45 +0300

    idk

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\journal_web_interactive\.venv
+set VIRTUAL_ENV=C:\journal_interactive\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
