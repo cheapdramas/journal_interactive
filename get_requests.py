4ed796ae1b8a20e12b75c573bb4de8e8287a2d0b
rostislav <bileckijrostislav46@gmail.com>
2024-06-04 21:29:44 +0300

    Update get_requests.py

--- get_requests.py
+++ get_requests.py
@@ -145,7 +145,7 @@ async def testing_request():
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
     }
     url = 'https://youtube.com'
-    req = requests.get(url,headers=headers)
+    req = requests.get(url,headers=headers,verify=False)
     return (url,req)
 
 
